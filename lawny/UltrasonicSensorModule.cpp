71b4ea6d449c8eebfca88073af5b38daa7124f19
Raymond <r2999haf@uwaterloo.ca>
2022-02-24 16:22:59 -0500

    fixed and working

--- lawny/UltrasonicSensorModule.cpp
+++ lawny/UltrasonicSensorModule.cpp
@@ -1,9 +1,64 @@
+#pragma once
+
 #include "UltrasonicSensorModule.hpp"
+
+NewPing sonar_front(sensor_front_trigger, sensor_front_echo, MAX_DISTANCE);
+NewPing sonar_front_left(sensor_front_left_trigger, sensor_front_left_echo, MAX_DISTANCE);
+NewPing sonar_left(sensor_left_trigger, sensor_left_echo, MAX_DISTANCE);
+NewPing sonar_back_left(sensor_back_left_trigger, sensor_back_left_echo, MAX_DISTANCE);
+NewPing sonar_back(sensor_back_trigger, sensor_back_echo, MAX_DISTANCE);
+NewPing sonar_back_right(sensor_back_right_trigger, sensor_back_right_echo, MAX_DISTANCE);
+NewPing sonar_right(sensor_right_trigger, sensor_right_echo, MAX_DISTANCE);
+NewPing sonar_front_right(sensor_front_right_trigger, sensor_front_right_echo, MAX_DISTANCE);
+
+int convertDirectionToIndex(directionDesignation direction) {
+    switch(direction) {
+        case directionDesignation::F:       
+            return 0;
+        case directionDesignation::FL:
+            return 1;
+        case directionDesignation::L:
+            return 2;
+        case directionDesignation::BL:
+            return 3;
+        case directionDesignation::B:
+            return 4;
+        case directionDesignation::BR:
+            return 5;
+        case directionDesignation::R:
+            return 6;
+        case directionDesignation::FR:
+            return 7;
+    }
+}
+
+String convertDirectionToName(directionDesignation direction) {
+    switch(direction) {
+        case directionDesignation::F:       
+            return "Front";
+        case directionDesignation::FL:
+            return "Front-Left";
+        case directionDesignation::L:
+            return "Left";
+        case directionDesignation::BL:
+            return "Back-Left";
+        case directionDesignation::B:
+            return "Back";
+        case directionDesignation::BR:
+            return "Back-Right";
+        case directionDesignation::R:
+            return "Right";
+        case directionDesignation::FR:
+            return "Front-Right";
+    }
+}
 
 UltrasonicSensorModule::UltrasonicSensorModule() {
     for (int i = 0; i < 8; i++) {
         UltrasonicSensorModule::sensorReadings[i] = 0;
     }
+    //pinMode(sensor_front_trigger, OUTPUT);
+    //pinMode(sensor_front_echo, INPUT);
 }
 
 UltrasonicSensorModule::~UltrasonicSensorModule() {
@@ -13,22 +68,24 @@ UltrasonicSensorModule::~UltrasonicSensorModule() {
 }
 
 int UltrasonicSensorModule::updateUltrasonicReadings() {
-    UltrasonicSensorModule::sensorReadings[0] = speedOfSoundConversion(sonar_front.ping_cm()); 
-    UltrasonicSensorModule::sensorReadings[1] = speedOfSoundConversion(sonar_front_left.ping_cm()); 
-    UltrasonicSensorModule::sensorReadings[2] = speedOfSoundConversion(sonar_left.ping_cm()); 
-    UltrasonicSensorModule::sensorReadings[3] = speedOfSoundConversion(sonar_back_left.ping_cm()); 
-    UltrasonicSensorModule::sensorReadings[4] = speedOfSoundConversion(sonar_back.ping_cm()); 
-    UltrasonicSensorModule::sensorReadings[5] = speedOfSoundConversion(sonar_back_right.ping_cm()); 
-    UltrasonicSensorModule::sensorReadings[6] = speedOfSoundConversion(sonar_right.ping_cm()); 
-    UltrasonicSensorModule::sensorReadings[7] = speedOfSoundConversion(sonar_front_right.ping_cm());
+    UltrasonicSensorModule::sensorReadings[0] = sonar_front.ping_cm(); 
+    UltrasonicSensorModule::sensorReadings[1] = sonar_front_left.ping_cm(); 
+    UltrasonicSensorModule::sensorReadings[2] = sonar_left.ping_cm(); 
+    UltrasonicSensorModule::sensorReadings[3] = sonar_back_left.ping_cm(); 
+    UltrasonicSensorModule::sensorReadings[4] = sonar_back.ping_cm(); 
+    UltrasonicSensorModule::sensorReadings[5] = sonar_back_right.ping_cm(); 
+    UltrasonicSensorModule::sensorReadings[6] = sonar_right.ping_cm(); 
+    UltrasonicSensorModule::sensorReadings[7] = sonar_front_right.ping_cm();
 }
 
 int UltrasonicSensorModule::checkDangerReading(directionDesignation direction) {
     int index = convertDirectionToIndex(direction);
-    if (UltrasonicSensorModule::sensorReadings[index] <= DANGER_DISTANCE) {
-        return -1;
+    int distance = UltrasonicSensorModule::sensorReadings[index];
+    if (distance <= DANGER_DISTANCE) {
         Serial.println("Sensor " + convertDirectionToName(direction) + " detected we are too close to an obstacle");
+        return -1;
     }
+    Serial.println(convertDirectionToName(direction) + " " + distance);
     return 0;
 }
 
@@ -71,5 +128,6 @@ int UltrasonicSensorModule::readUltrasonicSensor(CMD_ENUM c) {
             error_desig += UltrasonicSensorModule::checkDangerReading(directionDesignation::BR);
             error_desig += UltrasonicSensorModule::checkDangerReading(directionDesignation::B);
             break;
+    }
     return error_desig;
 }
--- lawny/UltrasonicSensorModule.hpp
+++ lawny/UltrasonicSensorModule.hpp
@@ -22,15 +22,6 @@
 #define sensor_front_right_echo 68
 #define sensor_front_right_trigger 69
 
-NewPing sonar_front(sensor_front_trigger, sensor_front_echo, MAX_DISTANCE);
-NewPing sonar_front_left(sensor_front_left_trigger, sensor_front_left_echo, MAX_DISTANCE);
-NewPing sonar_left(sensor_left_trigger, sensor_left_echo, MAX_DISTANCE);
-NewPing sonar_back_left(sensor_back_left_trigger, sensor_back_left_echo, MAX_DISTANCE);
-NewPing sonar_back(sensor_back_trigger, sensor_back_echo, MAX_DISTANCE);
-NewPing sonar_back_right(sensor_back_right_trigger, sensor_back_right_echo, MAX_DISTANCE);
-NewPing sonar_right(sensor_right_trigger, sensor_right_echo, MAX_DISTANCE);
-NewPing sonar_front_right(sensor_front_right_trigger, sensor_front_right_echo, MAX_DISTANCE);
-
 enum class directionDesignation {
     F,
     FL,
@@ -42,51 +33,6 @@ enum class directionDesignation {
     FR
 };
 
-int convertDirectionToIndex(directionDesignation direction) {
-    switch(direction) {
-        case directionDesignation::F:       
-            return 0;
-        case directionDesignation::FL:
-            return 1;
-        case directionDesignation::L:
-            return 2;
-        case directionDesignation::BL:
-            return 3;
-        case directionDesignation::B:
-            return 4;
-        case directionDesignation::BR:
-            return 5;
-        case directionDesignation::R:
-            return 6;
-        case directionDesignation::FR:
-            return 7;
-    }
-}
-
-String convertDirectionToName(directionDesignation direction) {
-    switch(direction) {
-        case directionDesignation::F:       
-            return "Front";
-        case directionDesignation::FL:
-            return "Front-Left";
-        case directionDesignation::L:
-            return "Left";
-        case directionDesignation::BL:
-            return "Back-Left";
-        case directionDesignation::B:
-            return "Back";
-        case directionDesignation::BR:
-            return "Back-Right";
-        case directionDesignation::R:
-            return "Right";
-        case directionDesignation::FR:
-            return "Front-Right";
-    }
-}
-int speedOfSoundConversion(int duration) {
-    return (duration / 2) * 0.0343;
-}
-
 class UltrasonicSensorModule {
     public:
         UltrasonicSensorModule();
@@ -97,5 +43,5 @@ class UltrasonicSensorModule {
         
     private:
         //index 0 is front, index 2 is left, index 4 is back, index 6 is right
-        int sensorReadings [8];
+        unsigned int sensorReadings [8];
 };
