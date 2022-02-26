9a7ad98f26c684b7faad5ba9a62aed2103b062cc
Raymond <r2999haf@uwaterloo.ca>
2022-02-26 13:42:15 -0500

    motor_blade

---
+++ lawny/MotorBladeModule.hpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+
+#define BLADE_DUTY_CYCLE 25
+
+#define BLADE_PWM 5 // PWM Frequency connected to Timer 1
+#define BLADE_DIR 4 // Generic Digital Output
+
+class MotorBladeModule {
+    public:
+        MotorBladeModule();
+        ~MotorBladeModule();
+        void startBladeWithPWM(int duty_cycle);
+        void startBlade();
+        void stopBlade();
+        void parkingBreak();
+};
