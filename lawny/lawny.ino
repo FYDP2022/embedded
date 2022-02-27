4985d2bf13a1db7a7cc8b7e9a4cebafc1ad8bf5d
Raymond <r2999haf@uwaterloo.ca>
2022-02-27 15:43:06 -0500

    minor

--- lawny/lawny.ino
+++ lawny/lawny.ino
@@ -2,6 +2,8 @@
 #include "CommunicationInterface.hpp"
 #include "UltrasonicSensorModule.hpp"
 #include "MotorControl.hpp"
+#include "MotorBladeModule.hpp"
+#include "RelayController.hpp"
 
 String inputString = "";         // a String to hold incoming data
 bool stringComplete = false;  // whether the string is complete
@@ -10,36 +12,39 @@ char cmd_string[50];
 
 //Unused for Now.
 CommunicationInterface interface = CommunicationInterface();
-char cmdInput [20];
+UltrasonicSensorModule ultrasonic = UltrasonicSensorModule();
+MotorController wheel_motors = MotorController();
+MotorBladeModule blade_motor = MotorBladeModule();
+RelayController relay = RelayController();
 
 void setup() {
   Serial.begin(115200);
-  memset(cmdInput, 0, 20);
+  memset(cmd_string, 0, 50);
 }
 
 
 //Will probably have to add code for calling modules in the loop
 void loop() {
   while(interface.isCommandAvailable() > 0) {
-    char module = interface.parseCommand(cmdInput);
-    Serial.println(cmdInput);
+    char module = interface.parseCommand(cmd_string);
+    Serial.println(cmd_string);
     switch (tolower(module)) {
       //Wheel Motors
       case 'w':
-        //motorController parseCommand method, passing cmdInput
+        wheel_motors.parse_command(cmd_string);
         break;
       
-      //Relay Motors
+      //Relay
       case 'r':
-        //Relay Motors parseCommand method, passing cmdInput
+        relay.parse_command(cmd_string);
         break;
       
       //Blade Motors
       case 'b':
-        //bladeController parseCommand method, passing cmdInput
+        blade_motor.parse_command(cmd_string);
         break;
     }
 
   }
-  memset(cmdInput, 0, 20);
+  memset(cmd_string, 0, 50);
 }
